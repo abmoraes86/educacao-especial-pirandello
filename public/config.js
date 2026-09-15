87a33f8336e4ee4d554ec9f831bb2a55a8696c27
abmoraes86 <abmoraes86@gmail.com>
2026-09-15 12:11:52 -0300

    chore: apontar aplicação para o novo projeto Supabase

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 // A chave publicável identifica o projeto; as permissões são verificadas no banco.
 // Nunca inserir chave secret ou service_role neste arquivo.
 window.ESCOLA_CONFIG = {
-  supabaseUrl: 'https://ioeuwiqruitxmwnhbqlb.supabase.co',
-  supabasePublishableKey: 'sb_publishable_fbQ74nSDEcBt5-WDHAjZ4Q_HcMU6TAI',
+  supabaseUrl: 'https://rijpfddlkkmrugztulhb.supabase.co',
+  supabasePublishableKey: 'sb_publishable_mdnQPksVPSp9z6AX8DDQLA_ukPpFSem',
 };
